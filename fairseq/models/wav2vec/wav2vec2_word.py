35aee1eaa3985450fc6e06b37d1fa9375c48f6e4
Marianne <marianne.metais@grenoble-inp.org>
2023-02-09 11:42:03 +0100

    Add w2v2 with word boundaries

--- fairseq/models/wav2vec/wav2vec2_word.py
+++ fairseq/models/wav2vec/wav2vec2_word.py
@@ -20,6 +20,8 @@ from fairseq.models.wav2vec.wav2vec2 import (
 )
 from fairseq.modules import LayerNorm
 from fairseq.tasks import FairseqTask
+from random import sample
+import numpy as np
 
 
 @dataclass
@@ -104,15 +106,201 @@ class Wav2Vec2Word(Wav2Vec2Model):
         padding_count=None,
         boundaries=None
     ):
-        source=torch.cat(source,dim=0).cuda()
+        if isinstance(source, list):
+            source=torch.cat(source,dim=0).cuda()
 
-        return super().forward(
-            source,
-            padding_mask,
-            mask,
-            features_only,
-            layer,
-            mask_indices,
-            mask_channel_indices,
-            padding_count,
-        )
+        if self.feature_grad_mult > 0:
+            features = self.feature_extractor(source)
+            if self.feature_grad_mult != 1.0:
+                features = GradMultiply.apply(features, self.feature_grad_mult)
+        else:
+            with torch.no_grad():
+                features = self.feature_extractor(source)
+
+        features_pen = features.float().pow(2).mean()
+
+        features = features.transpose(1, 2)
+        features = self.layer_norm(features)
+        unmasked_features = features.clone()
+
+        if padding_mask is not None and padding_mask.any():
+            input_lengths = (1 - padding_mask.long()).sum(-1)
+            # apply conv formula to get real output_lengths
+            output_lengths = self._get_feat_extract_output_lengths(input_lengths)
+
+            padding_mask = torch.zeros(
+                features.shape[:2], dtype=features.dtype, device=features.device
+            )
+
+            # these two operations makes sure that all values
+            # before the output lengths indices are attended to
+            padding_mask[
+                (
+                    torch.arange(padding_mask.shape[0], device=padding_mask.device),
+                    output_lengths - 1,
+                )
+            ] = 1
+            padding_mask = (1 - padding_mask.flip([-1]).cumsum(-1).flip([-1])).bool()
+        else:
+            padding_mask = None
+
+        time_steps_to_drop = features.size(1) % self.crop_seq_to_multiple
+        if time_steps_to_drop != 0:
+            features = features[:, :-time_steps_to_drop]
+            unmasked_features = unmasked_features[:, :-time_steps_to_drop]
+            if padding_mask is not None:
+                padding_mask = padding_mask[:, :-time_steps_to_drop]
+
+        if self.post_extract_proj is not None:
+            features = self.post_extract_proj(features)
+
+        features = self.dropout_input(features)
+        unmasked_features = self.dropout_features(unmasked_features)
+
+        num_vars = None
+        code_ppl = None
+        prob_ppl = None
+        curr_temp = None
+
+        if self.input_quantizer:
+            q = self.input_quantizer(features, produce_targets=False)
+            features = q["x"]
+            num_vars = q["num_vars"]
+            code_ppl = q["code_perplexity"]
+            prob_ppl = q["prob_perplexity"]
+            curr_temp = q["temp"]
+            features = self.project_inp(features)
+
+        if mask:
+            if boundaries is not None:
+                mask_indices = self.compute_mask_indices_with_word_boundaries(features.shape, boundaries)
+                mask_indices = torch.from_numpy(mask_indices).to(features.device)
+            else:
+                mask_indices = None
+
+            x, mask_indices = self.apply_mask(
+                features,
+                padding_mask,
+                mask_indices=mask_indices,
+                mask_channel_indices=mask_channel_indices,
+            )
+            y = unmasked_features
+        else:
+            x = features
+            y = unmasked_features
+            mask_indices = None
+
+        x, layer_results = self.encoder(x, padding_mask=padding_mask, layer=layer)
+
+        if features_only:
+            return {
+                "x": x,
+                "padding_mask": padding_mask,
+                "features": unmasked_features,
+                "layer_results": layer_results,
+            }
+
+        if self.quantizer:
+            if self.negatives_from_everywhere:
+                q = self.quantizer(unmasked_features, produce_targets=False)
+                y = q["x"]
+                num_vars = q["num_vars"]
+                code_ppl = q["code_perplexity"]
+                prob_ppl = q["prob_perplexity"]
+                curr_temp = q["temp"]
+                y = self.project_q(y)
+
+                negs, _ = self.sample_negatives(
+                    y,
+                    mask_indices[0].sum(),
+                    padding_count=padding_count,
+                )
+                y = y[mask_indices].view(y.size(0), -1, y.size(-1))
+
+            else:
+                q = self.quantizer(y, produce_targets=False)
+                y = q["x"]
+                num_vars = q["num_vars"]
+                code_ppl = q["code_perplexity"]
+                prob_ppl = q["prob_perplexity"]
+                curr_temp = q["temp"]
+
+                y = self.project_q(y)
+
+                negs, _ = self.sample_negatives(
+                    y,
+                    y.size(1),
+                    padding_count=padding_count,
+                )
+
+            if self.codebook_negatives > 0:
+                cb_negs = self.quantizer.sample_from_codebook(
+                    y.size(0) * y.size(1), self.codebook_negatives
+                )
+                cb_negs = cb_negs.view(
+                    self.codebook_negatives, y.size(0), y.size(1), -1
+                )  # order doesnt matter
+                cb_negs = self.project_q(cb_negs)
+                negs = torch.cat([negs, cb_negs], dim=0)
+        else:
+            y = self.project_q(y)
+
+            if self.negatives_from_everywhere:
+                negs, _ = self.sample_negatives(
+                    unmasked_features,
+                    y.size(1),
+                    padding_count=padding_count,
+                )
+                negs = self.project_q(negs)
+            else:
+                negs, _ = self.sample_negatives(
+                    y,
+                    y.size(1),
+                    padding_count=padding_count,
+                )
+
+        if self.target_glu:
+            y = self.target_glu(y)
+            negs = self.target_glu(negs)
+
+        x = self.final_proj(x)
+        x = self.compute_preds(x, y, negs)
+
+        result = {
+            "x": x,
+            "padding_mask": padding_mask,
+            "features_pen": features_pen,
+        }
+
+        if prob_ppl is not None:
+            result["prob_perplexity"] = prob_ppl
+            result["code_perplexity"] = code_ppl
+            result["num_vars"] = num_vars
+            result["temp"] = curr_temp
+
+        return result
+
+    def compute_mask_indices_with_word_boundaries(
+        self,
+        shape: tuple,
+        boundaries: List,
+        mask_prob = 0.15
+    ):
+        bsz, size, _ = shape
+        mask = np.full((bsz, size), False)
+        mask_idcs = [[]] * bsz
+        for batch_idx, b_boundaries in enumerate(boundaries):
+            nb_of_masked_words = int(mask_prob*len(b_boundaries))
+            masks = sample(b_boundaries, nb_of_masked_words)
+            mask_indices = [[int(time/0.020) for time in mask] for mask in masks]
+            for start, stop in mask_indices:
+                mask_idcs[batch_idx].extend([i for i in range(start, stop)])
+                mask[batch_idx, start:stop] = True
+
+        # min_len = min([len(m) for m in mask_idcs])
+        # for i, mask_idc in enumerate(mask_idcs):
+        #     if len(mask_idc) > min_len:
+        #         mask_idc = np.random.choice(mask_idc, min_len, replace=False)
+        #     mask[i, mask_idc] = True
+   
+        return mask
